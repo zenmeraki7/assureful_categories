41fd5987f77263584fcd4b55dc0f89dc7ec819a7
zenmeraki7 <zenmerakihelp@gmail.com>
2025-11-03 15:15:06 +0530

    Remove venv and add .gitignore

--- venv/Lib/site-packages/sentence_transformers/sparse_encoder/callbacks/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from .splade_callbacks import SchedulerType, SpladeRegularizerWeightSchedulerCallback
-
-__all__ = ["SpladeRegularizerWeightSchedulerCallback", "SchedulerType"]
--- venv/Lib/site-packages/sentence_transformers/sparse_encoder/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-from .MLMTransformer import MLMTransformer
-from .SparseAutoEncoder import SparseAutoEncoder
-from .SparseStaticEmbedding import SparseStaticEmbedding
-from .SpladePooling import SpladePooling
-
-__all__ = ["SparseAutoEncoder", "MLMTransformer", "SpladePooling", "SparseStaticEmbedding"]
